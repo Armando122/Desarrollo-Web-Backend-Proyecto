fb6143f6533e12c58cf1f701a8e5a3c7784576b1
armando122 <armandoramirezg@ciencias.unam.mx>
2022-11-27 01:26:05 -0600

    Agregando muestra de código para feign

--- invoice/src/main/java/com/invoice/api/service/SvcCartImp.java
+++ invoice/src/main/java/com/invoice/api/service/SvcCartImp.java
@@ -1,6 +1,6 @@
-/*package com.invoice.api.service;*/
+package com.invoice.api.service;
 
-/*import java.util.List;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,40 +28,40 @@ public class SvcCartImp implements SvcCart {
 		return repo.findByRfcAndStatus(rfc,1);
 	}
 
-	/*@Override
+	@Override
 	public ApiResponse addToCart(Cart cart) {
 		if(!validateCustomer(cart.getRfc()))
-			throw new ApiException(HttpStatus.BAD_REQUEST, "customer does not exist");*/
+			throw new ApiException(HttpStatus.BAD_REQUEST, "customer does not exist");
 			
 		/*
 		 * Requerimiento 3
 		 * Validar que el GTIN exista. Si existe, asignar el stock del producto a la variable product_stock 
 		 */
-		/*Integer product_stock = 0; // cambiar el valor de cero por el stock del producto recuperado de la API Product
+		Integer product_stock = 0; // cambiar el valor de cero por el stock del producto recuperado de la API Product
 		
 		if(cart.getQuantity() > product_stock) {
 			throw new ApiException(HttpStatus.BAD_REQUEST, "invalid quantity");
-		}*/
+		}
 		
 		/*
 		 * Requerimiento 4
 		 * Validar si el producto ya había sido agregado al carrito para solo actualizar su cantidad
 		 */
 		
-		/*cart.setStatus(1);
+		cart.setStatus(1);
 		repo.save(cart);
 		return new ApiResponse("item added");
-	}*/
+	}
 
-	/*@Override
+	@Override
 	public ApiResponse removeFromCart(Integer cart_id) {
 		if (repo.removeFromCart(cart_id) > 0)
 			return new ApiResponse("item removed");
 		else
 			throw new ApiException(HttpStatus.BAD_REQUEST, "item cannot be removed");
-	}*/
+	}
 
-	/*@Override
+	@Override
 	public ApiResponse clearCart(String rfc) {
 		if (repo.clearCart(rfc) > 0)
 			return new ApiResponse("cart removed");
@@ -79,6 +79,6 @@ public class SvcCartImp implements SvcCart {
 		}catch(Exception e) {
 			throw new ApiException(HttpStatus.BAD_REQUEST, "unable to retrieve customer information");
 		}
-	}*/
+	}
 
-/*}*/
+}
